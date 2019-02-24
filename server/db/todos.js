7680fda8a61b073f5e6a6baa21e30a1c1a20d945
Richard Graham <richgrahamdev@gmail.com>
2019-02-25 10:58:10 +1300

    back end done

--- server/db/todos.js
+++ server/db/todos.js
@@ -1,1 +1,32 @@
 const connection = require('./connection')
+
+function getTodos(testDb) {
+    const db = testDb || connection 
+
+    return db('todos')
+}
+
+function getTodosByPriority(priority, testDb) {
+    const db = testDb || connection 
+
+    return db('todos').where('priority', priority)
+}
+
+function getTodosByCategory(category, testDb) {
+    const db = testDb || connection 
+
+    return db('todos').where('category', category)
+}
+
+function getTodosByCompleted(is_complete, testDb) {
+    const db = testDb || connection 
+
+    return db('todos').where('is_complete', is_complete)
+}
+
+module.exports = {
+    getTodos,
+    getTodosByPriority,
+    getTodosByCategory,
+    getTodosByCompleted
+}
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,17 +1,52 @@
 const express = require('express')
 const router = express.Router()
+const {getTodos, getTodosByPriority, getTodosByCategory, getTodosByCompleted} = require('../db/todos')
 
 // GET api/v1/tweets
 router.get('/', (req, res) => {
-    res.json([
-        {
-            task: 'Go shopping',
-            priority: 3,
-            category: 'Home',
-            is_completed: false,
-            due_at: 12/10/2019
-        }
-    ])
+   getTodos()
+   .then(todos => {
+       res.json(todos)
+   })
+   .catch(err => {
+       console.log(err)
+       res.status(500).json({error: 'Something went wrong'})
+   })
+})
+
+router.get('/priority/:priority', (req, res) => {
+    getTodosByPriority(req.params.priority)
+    .then(todos => {
+        res.json(todos)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'Something went wrong'})
+    })
+})
+
+router.get('/category/:category', (req, res) => {
+    getTodosByCategory(req.params.category)
+    .then(todos => {
+        res.json(todos)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'Something went wrong'})
+    })
+})
+
+router.get('/complete/:is_complete', (req, res) => {
+    req.params.is_complete == 'true' ? req.params.is_complete = 1 : ''
+    req.params.is_complete == 'false' ? req.params.is_complete = 0 : ''
+    getTodosByCompleted(req.params.is_complete)
+    .then(todos => {
+        res.json(todos)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'Something went wrong'})
+    })
 })
 
 
