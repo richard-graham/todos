14ee2969c11207bcd94fadb49cea779fa918508e
Richard Graham <richgrahamdev@gmail.com>
2019-02-25 17:45:26 +1300

    fixed date (hopefully)

--- seeds/todos.js
+++ seeds/todos.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('todos').insert([
-        {id: 1, task: 'Mow the lawns', priority: '2',category: 'home', is_complete: false, due_at: '29-02-2018'},
+        {id: 1, task: 'Mow the lawns', priority: '2',category: 'home', is_complete: false, due_at: '28-02-2018'},
         {id: 2, task: 'Deliver quarterly report', priority: '4',category: 'work', is_complete: false, due_at: '04-03-2018'},
         {id: 3, task: 'Replace golf balls', priority: '1',category: 'leisure', is_complete: true, due_at: '26-02-2018'}
       ]);
